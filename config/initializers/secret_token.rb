b39c018f4b5be4d71cb645b1415d48c8bda66b09
Marnix Kok <jmkok@fourdsoftware.com>
2012-06-25 14:11:33 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoutzor::Application.config.secret_token = '44c77a9481a84b6925c968825e3e963bd3ac40a350e814dafdab80198c929db3f62536ce9c5be0cf83b33dcf7694a00fefb48337fe4d2de6e89456f8e192cb26'
